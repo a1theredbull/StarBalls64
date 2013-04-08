7fe137b23f17f1e1eba12031c6e1019e616eceba
Alex Chau <chaua@mail.gvsu.edu>
2013-04-07 22:24:36 -0400

    Fixed wireframe shapes, got rid of sun

--- gl-scripts.js
+++ gl-scripts.js
@@ -1,8 +1,8 @@
 var scene, camera, renderer;
-var ship, targetSun;
+var ship;
 var shapes = [];
-var numShapes = 100;
-var directionalLight;
+var numShapes = 170;
+var directionalLight, shipLight;
 var shapeMaterial;
 
 function init() {
@@ -19,17 +19,11 @@ function init() {
 	document.body.appendChild(renderer.domElement);
 
 	/* OBJECTS */
-	var shipMaterial = new THREE.MeshLambertMaterial({color: 0x172E00});
-	var shapeMaterial = new THREE.MeshLambertMaterial({color: 0xE34F12});
-	var sunMaterial = new THREE.MeshLambertMaterial({color: 0xFFFF00});
+	var shipMaterial = new THREE.MeshPhongMaterial({color: 0x172E00});
 
-	ship = new THREE.Mesh(new THREE.CylinderGeometry(80,80,80,6,1,false), shipMaterial);
+	ship = new THREE.Mesh(new THREE.CylinderGeometry(60, 80, 30, 8, 1, false), shipMaterial);
 	ship.position.z = 700;
 	scene.add(ship);
-
-	targetSun = new THREE.Mesh(new THREE.SphereGeometry(500, 200, 200), sunMaterial);
-	targetSun.position.set(0,0,-8000);
-	scene.add(targetSun);
 
 	generateShapes();
 
@@ -40,9 +34,15 @@ function init() {
 	var ambientLight = new THREE.AmbientLight(0x111111);
 	scene.add(ambientLight);
 
-	directionalLight = new THREE.PointLight(0xFFFFFF, 3);
-	directionalLight.position.set(10, 0, -500);
+	shipLight = new THREE.SpotLight(0xFFFFFF, 6, 6000, true);
+	shipLight.position.z = 800;
+	shipLight.shadowCameraFov = 100;
+	scene.add(shipLight);
+
+	directionalLight = new THREE.PointLight(0xFFFFFF, 0.2);
+	directionalLight.position.set(0, 0, 750);
 	scene.add(directionalLight);
+	
 
 	render();
 }
@@ -53,44 +53,42 @@ init();
 function render() {
 	requestAnimationFrame(render);
 	ship.rotation.y += 0.02;
-	targetSun.position.z += 1;
-	if(targetSun.position.z > 1000) {
-		targetSun.position.z = -8000;
-	}
 
 	for(var i = 0; i < shapes.length; i++) {
 		shapes[i].position.z += shapes[i].speed;
-		shapes[i].rotation.x += 0.03;
+		shapes[i].rotation.x += 0.2;
 
 		if(shapes[i].position.z > 1000) {
-			shapes[i].position.x = Math.random() * 5600 - 2800;
-			shapes[i].position.y = Math.random() * 3200 - 1600;
-			shapes[i].position.z = -6000;
+			shapes[i].position.x = Math.random() * 7000 - 3500; //5600-2800
+			shapes[i].position.y = Math.random() * 5000 - 2500; //3200-1600
+			shapes[i].position.z = -8000;
 		}
 	}
 	renderer.render(scene, camera);
 }
 
-var mouseY;
-
 function handleMouseMove(event) {
-	mouseY = event.clientY;
 	ship.position.x = (event.clientX - 700) * 0.8;
 	ship.position.y = -(event.clientY - 350) * 0.8;
+	shipLight.position.x = directionalLight.position.x = -ship.position.x;
+	shipLight.position.y = directionalLight.position.y = -ship.position.y;
 }
 
 function generateShapes() {
 	var shape;
-	var randX, randY, randZ;
+	var randX, randY, randZ, randColor;
 
 	for(var i = 0; i < numShapes; i++) {
-		randX = Math.random() * 5600 - 2800; //-1400 to 1400
-		randY = Math.random() * 3200 - 1600; //-800 to 800
-		randZ = Math.random() * -6000 - 2000;
+		randX = Math.random() * 10000 - 5000; //-1400 to 1400
+		randY = Math.random() * 6000 - 300; //-800 to 800
+		randZ = Math.random() * -6000 - 4000;
+		randColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16); //gets random color
 
-		shape = new THREE.Mesh(new THREE.SphereGeometry(60, 8, 8), shapeMaterial);
-		shape.speed = Math.random() * 60 + 20;
+		shapeMaterial = new THREE.MeshLambertMaterial({color: randColor });
+		shape = new THREE.Mesh(new THREE.SphereGeometry(60, 10, 10), shapeMaterial);
+		shape.speed = Math.random() * 260 + 10;
 		shape.position.set(randX, randY, randZ)
+
 		scene.add(shape);
 		shapes.push(shape);
 	}
